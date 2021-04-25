dffe63f24e62b4903109f051adaaf5a811bebeaf
nugyn <linhduyn@gmail.com>
2021-04-25 20:30:28 +1000

    Init NuBankAPI Project

---
+++ BankTransaction.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace NuBankAPI
+{
+    public class BankTransaction
+    {
+        
+    }
+}
---
+++ Controllers/BankAPIController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NuBankAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BankAPIController : ControllerBase
+    {
+        private readonly ILogger<BankAPIController> _logger;
+
+        public BankAPIController(ILogger<BankAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<BankTransaction> Get()
+        {
+            //TODO
+            return null;
+        }
+    }
+}
